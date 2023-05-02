2624853427bbf890a9a08e3da169527908100042
Austin Simpson <asimpson@chapman.edu>
2023-05-02 11:38:45 -0700

    Initial commit

---
+++ components/Forecast.js
@@ -0,0 +1,77 @@
+import styled from "styled-components"
+import Spinner from "./Spinner"
+
+export default function Forecast({ data, error }) {
+
+    if(error) {
+        return <Wrapper>
+            <h1>Oops! No weather today.</h1>
+        </Wrapper>
+    }
+
+    if (!data) {
+        return <Wrapper>
+            <Spinner />
+        </Wrapper>
+    }
+
+
+    return <Wrapper>
+        <Description>{data?.current?.condition?.text}</Description>
+        <Temp>{data?.current?.temp_f}°F</Temp>
+        <Location>{data?.location?.name}, {data?.location?.region}</Location>
+    </Wrapper>
+}
+const Wrapper = styled.div`
+    height: 100%;
+    border-radius: 10px;
+    font-size: 2rem;
+    line-height: .8;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    color: white;
+    background-image: linear-gradient(
+        340deg,
+        hsl(217deg 77% 75%) 0%,
+        hsl(218deg 77% 73%) 8%,
+        hsl(219deg 78% 72%) 17%,
+        hsl(219deg 78% 70%) 26%,
+        hsl(220deg 78% 69%) 35%,
+        hsl(220deg 78% 67%) 44%,
+        hsl(221deg 78% 66%) 52%,
+        hsl(222deg 77% 64%) 61%,
+        hsl(223deg 77% 63%) 69%,
+        hsl(224deg 76% 62%) 76%,
+        hsl(225deg 75% 60%) 83%,
+        hsl(226deg 74% 59%) 89%,
+        hsl(227deg 72% 58%) 95%,
+        hsl(229deg 71% 57%) 100%
+      );
+`
+
+const Icon = styled.img`
+   display: block;
+   width: 20rem;
+`
+const Temp = styled.h2`
+   font-size: 10rem;
+   margin: 0;
+`
+
+const Description = styled.h2`
+    marign: 0;
+    font-weight: 300;
+    letter-spacing: 2px;
+    text-transform: uppercase;
+`
+
+const Location = styled.h2`
+    marign: 0;
+    font-weight: 300;
+    text-transform: uppercase;
+    font-size: 1rem;
+    letter-spacing: 2px;
+    padding-top: 10px;
+`
---
+++ pages/index.js
@@ -0,0 +1,69 @@
+import styled from 'styled-components'
+import { useState } from 'react'
+import useSWR, { SWRConfig } from 'swr'
+import Forecast from '../components/Forecast'
+
+const books = []
+// change fetcher to look at booklist.json
+
+const fetcher = url => fetch(url).then(r => r.json())
+
+
+
+export default function Home({ fallback }) {
+  const [city, setCity] = useState("Los Angeles")
+  const { data, error } = useSWR(`/api/weather/${city}`, fetcher)
+
+  return (
+    <SWRConfig value={{ fallback }}>
+      <Wrapper>
+      <Cities>
+        { cities.map(c => (
+          <Button selected={c === city} key={c} onClick={() => setCity(c)}>{c}</Button>
+        ))}
+      </Cities>
+      <ForecastWrapper>
+        <Forecast data={data} error={error} />
+      </ForecastWrapper>
+      </Wrapper>
+      </SWRConfig>)
+}
+
+export async function getServerSideProps(context) {
+  const key = process.env.WEATHER_API_KEY
+  const city = cities[0]
+  const url = `http://api.weatherapi.com/v1/current.json?key=${key}&q=${city}&aqi=no`
+  const response = await fetch(url)
+  const data = await response.json()
+  
+  return {
+    props: { data }
+  }
+}
+
+const Cities = styled.div`
+  padding-top: 25px;
+  display: flex;
+  gap: 10px;
+  justify-content: center;
+`
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`
+
+const ForecastWrapper = styled.div`
+  flex: 1;
+  margin: 50px;
+  margin-top: 25px;
+`
+
+const Button = styled.button`
+  border: none;
+  background-color: transparent;
+  font-size: 1rem;
+  padding: 5px;
+  border-bottom: 2px solid ${(p) => p.selected? 'black' : 'transparent'} ;
+`
